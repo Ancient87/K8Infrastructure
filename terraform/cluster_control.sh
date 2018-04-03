68847475f342c6281b0380054efe55810a376b43
ancient87 <Ancient19@gmail.com>
2018-04-03 15:55:39 +0200

    Fixed cluster_control script and added comments

--- terraform/cluster_control.sh
+++ terraform/cluster_control.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Variables that define how many masters ELBs and nodes there will be
 cluster_up=0
 nodes_count=0
 OPTS=`getopt -o ud --long up,down -- "$@"`
@@ -18,8 +19,10 @@ while true; do
 done
 echo 'cluster_up='${cluster_up}
 echo 'nodes_count='${nodes_count}
-#terraform plan -var="cluster_up=${cluster_up}" -var="nodes_count=${nodes_count}"
+# Use apply to bring up or down the cluster
+terraform apply -var="cluster_up=${cluster_up}" -var="nodes_count=${nodes_count}" -auto-approve
 ELB_DNS=$(terraform output elb_ip)
 echo ${ELB_DNS}
-kubectl config set-cluster $NAME --server="${ELB_DNS}" --insecure-skip-tls-verify
+# Set kubectl to target API server on created ELB
+kubectl config set-cluster $NAME --server="https://${ELB_DNS}" --insecure-skip-tls-verify
 
